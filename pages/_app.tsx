f9bd5d1ff08a66dfdeb78d48d19066ca9440e3e7
Özgür Isbert <oezguerisbert@gmail.com>
2022-10-26 10:49:55 +0200

    mantine v5.6.2

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,26 +1,30 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import { AppProps } from 'next/app'
+import Head from 'next/head'
 import { MantineProvider } from '@mantine/core'
 
-function MyApp({ Component, pageProps }: AppProps) {
+export default function App(props: AppProps) {
+  const { Component, pageProps } = props
+
   return (
-    <MantineProvider
-      withGlobalStyles
-      withNormalizeCSS
-      theme={{
-        colorScheme: 'light',
-        breakpoints: {
-          xs: 500,
-          sm: 800,
-          md: 1000,
-          lg: 1200,
-          xl: 1400,
-        },
-      }}
-    >
-      <Component {...pageProps} />
-    </MantineProvider>
+    <>
+      <Head>
+        <title>Page title</title>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
+      </Head>
+
+      <MantineProvider
+        withGlobalStyles
+        withNormalizeCSS
+        theme={{
+          /** Put your mantine theme override here */
+          colorScheme: 'light',
+        }}
+      >
+        <Component {...pageProps} />
+      </MantineProvider>
+    </>
   )
 }
-
-export default MyApp
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,20 +1,25 @@
-import { createGetInitialProps } from '@mantine/next'
-import Document, { Head, Html, Main, NextScript } from 'next/document'
+import Document, { DocumentContext } from 'next/document'
+import { ServerStyles, createStylesServer } from '@mantine/next'
 
-const getInitialProps = createGetInitialProps()
+// optional: you can provide your cache as a fist argument in createStylesServer function
+const stylesServer = createStylesServer()
 
 export default class _Document extends Document {
-  static getInitialProps = getInitialProps
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx)
 
-  render() {
-    return (
-      <Html>
-        <Head />
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    )
+    // Add your app specific logic here
+
+    return {
+      ...initialProps,
+      styles: [
+        initialProps.styles,
+        <ServerStyles
+          html={initialProps.html}
+          server={stylesServer}
+          key="styles"
+        />,
+      ],
+    }
   }
 }
